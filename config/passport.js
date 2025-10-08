4b4d00483b89af5ec38c9977bd3bb05be9e9f2db
SavelevAAlek <savelyev.main@gmail.com>
2025-10-08 21:38:32 +0600

    api change

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,23 @@
 const passport = require('passport');
 const VKontakteStrategy = require('passport-vkontakte').Strategy;
+const crypto = require('crypto');
+
+// Генерация PKCE параметров
+function generatePKCE() {
+  const codeVerifier = crypto.randomBytes(32).toString('base64url');
+  const codeChallenge = crypto.createHash('sha256').update(codeVerifier).digest('base64url');
+  return { codeVerifier, codeChallenge };
+}
 
 // Настройка стратегии ВКонтакте
 passport.use(new VKontakteStrategy({
   clientID: process.env.VK_APP_ID,
   clientSecret: process.env.VK_APP_SECRET,
   callbackURL: process.env.VK_CALLBACK_URL || "http://localhost:3000/auth/vk/callback",
-  scope: ['email', 'photos', 'wall', 'friends']
+  scope: ['email', 'photos', 'wall', 'friends'],
+  // Используем новый API ВК
+  authorizationURL: 'https://id.vk.ru/authorize',
+  tokenURL: 'https://id.vk.ru/oauth2/auth'
 }, (accessToken, refreshToken, params, profile, done) => {
   // Здесь обычно происходит сохранение пользователя в БД
   // Пока используем простую структуру в памяти
